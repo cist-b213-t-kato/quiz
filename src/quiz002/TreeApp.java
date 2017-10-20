a0aa0e3568cdf092b6853617ce1bcbeea6dabb28
TatsumiKato <tkato@TatsumiKatonoMacBook-Air.local>
2017-10-20 18:37:17 +0900

    つりー問題

---
+++ src/quiz002/TreeApp.java
@@ -0,0 +1,18 @@
+package quiz002;
+
+public class TreeApp {
+	public static void main(String[] args) {
+//		以下のように表示せよ
+//		*****
+//		****
+//		***
+//		**
+//		*
+//		*
+//		**
+//		***
+//		****
+//		*****
+
+	}
+}
