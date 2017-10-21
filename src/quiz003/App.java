e52bdde5b44f1845d3dc6a2ac127d7afe45be141
TatsumiKato <tkato@TatsumiKatonoMacBook-Air.local>
2017-10-21 18:39:10 +0900

    問題追加

---
+++ src/quiz003/App.java
@@ -0,0 +1,13 @@
+package quiz003;
+
+public class App {
+	public static void main(String[] args) {
+
+		Cat[] cats = new Cat[3];
+
+		cats[0] = new Cat("いちご", "スコティッシュフォールド");
+		cats[1] = new Cat("ふたば", "ロシアンブルー");
+		cats[2] = new Cat("みけ", "三毛猫");
+
+	}
+}
---
+++ src/quiz003/Cat.java
@@ -0,0 +1,17 @@
+package quiz003;
+
+public class Cat {
+
+	private String name;
+	private String breed;
+
+	public Cat(String name, String breed) {
+		this.name = name;
+		this.breed = breed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
